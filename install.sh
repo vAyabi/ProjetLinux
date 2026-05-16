d62614d7e73ab389f5a0c6feec5cc12d3ddd06c5
tiki-git <antiki.djibaba@gmail.com>
2026-05-16 22:40:58 +0200

    Add initial install script with configuration variables

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+# variables
+DISK="/dev/sda"
+PASSWORD="azerty123"
+HOSTNAME="archlinux"
+TIMEZONE="Europe/Paris"
+LOCALE="fr_FR.UTF-8"
+KEYMAP="fr"
+
+USER_MAIN="collegue"
+USER_SON="fils"
+GROUP_SHARED="famille"
+
+MOUNT_VBOX="/opt/vbox"
+MOUNT_SHARED="/srv/shared"
